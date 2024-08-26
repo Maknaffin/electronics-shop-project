0f3ef819fd977e2193a4391c951fac6591ea4c76
Timofey <tima.khlystov99@mail.ru>
2024-08-26 21:24:58 +0300

    Completed task 4

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,5 +1,6 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 from src.item import Item
+from src.phone import Phone
 
 
 def test_calculate_total_price():
@@ -46,9 +47,20 @@ def test_string_to_number():
 
 def test_str_method():
     item1 = Item("Смартфон", 10000, 20)
+    phone1 = Phone("iPhone 14", 120_000, 5, 2)
+    assert str(phone1) == 'iPhone 14'
     assert str(item1) == 'Смартфон'
 
 
 def test_repr_method():
     item1 = Item("Смартфон", 10000, 20)
+    phone1 = Phone("iPhone 14", 120_000, 5, 2)
+    assert repr(phone1) == "Phone('iPhone 14', 120000, 5, 2)"
     assert repr(item1) == "Item('Смартфон', 10000, 20)"
+
+
+def test_add_method():
+    item1 = Item("Смартфон", 10000, 20)
+    phone1 = Phone("iPhone 14", 120_000, 5, 2)
+    assert item1 + phone1 == 25
+    assert phone1 + phone1 == 10
